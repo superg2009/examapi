4f858299cfcefb61fd8c7d97e7cd35d15a1eb14b
Grant Marshall <grantrmarshall97@gmail.com>
2021-12-03 19:07:09 -0800

    folder name change and server react from flask

--- api/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export FLASK_APP=src/app.py
-pipenv shell
-pipenv lock -r >requirements.txt
--- api/src/app.py
+++ api/src/app.py
@@ -1,14 +1,19 @@
-from flask import Flask,jsonify,request
+from flask import Flask,jsonify,request,send_from_directory
 from flask_cors import CORS
 from src.examscrap import get_schedule
+import os
 
-app = Flask(__name__)
+#ohh this is such a hack i hate it
+path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))),'client/build')
+
+app = Flask(__name__,static_folder=path,static_url_path='')
 cors = CORS(app)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World"
+    print(app.static_folder)
+    return send_from_directory(app.static_folder,'index.html')
 
 @app.route('/api/schedule', methods=['GET'])
 def return_schedule():
